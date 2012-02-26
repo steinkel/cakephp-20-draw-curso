509f5b06fd60062e597301365aac5d51534584a4
steinkel <steinkel@gmail.com>
2012-02-26 18:09:12 +0000

    added not scaffolded Ofertas

--- Controller/OfertasController.php
+++ Controller/OfertasController.php
@@ -3,14 +3,100 @@ App::uses('AppController', 'Controller');
 /**
  * Ofertas Controller
  *
+ * @property Oferta $Oferta
  */
 class OfertasController extends AppController {
 
-/**
- * Scaffold
- *
- * @var mixed
- */
-	public $scaffold;
 
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Oferta->recursive = 0;
+		$this->set('ofertas', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		$this->Oferta->id = $id;
+		if (!$this->Oferta->exists()) {
+			throw new NotFoundException(__('Invalid oferta'));
+		}
+		$this->set('oferta', $this->Oferta->read(null, $id));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Oferta->create();
+			if ($this->Oferta->save($this->request->data)) {
+				$this->Session->setFlash(__('The oferta has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The oferta could not be saved. Please, try again.'));
+			}
+		}
+		$empresas = $this->Oferta->Empresa->find('list');
+		$focos = $this->Oferta->Foco->find('list');
+		$this->set(compact('empresas', 'focos'));
+	}
+
+/**
+ * edit method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		$this->Oferta->id = $id;
+		if (!$this->Oferta->exists()) {
+			throw new NotFoundException(__('Invalid oferta'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Oferta->save($this->request->data)) {
+				$this->Session->setFlash(__('The oferta has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The oferta could not be saved. Please, try again.'));
+			}
+		} else {
+			$this->request->data = $this->Oferta->read(null, $id);
+		}
+		$empresas = $this->Oferta->Empresa->find('list');
+		$focos = $this->Oferta->Foco->find('list');
+		$this->set(compact('empresas', 'focos'));
+	}
+
+/**
+ * delete method
+ *
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		$this->Oferta->id = $id;
+		if (!$this->Oferta->exists()) {
+			throw new NotFoundException(__('Invalid oferta'));
+		}
+		if ($this->Oferta->delete()) {
+			$this->Session->setFlash(__('Oferta deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Oferta was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
 }
--- Test/Case/Controller/OfertasControllerTest.php
+++ Test/Case/Controller/OfertasControllerTest.php
@@ -1,5 +1,5 @@
 <?php
-/* Ofertas Test cases generated on: 2012-02-26 18:03:04 : 1330275784*/
+/* Ofertas Test cases generated on: 2012-02-26 19:07:29 : 1330279649*/
 App::uses('OfertasController', 'Controller');
 
 /**
@@ -59,6 +59,51 @@ class OfertasControllerTestCase extends CakeTestCase {
 		unset($this->Ofertas);
 
 		parent::tearDown();
+	}
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+
 	}
 
 }
